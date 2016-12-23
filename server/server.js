08118a979076351af76962e20bd8894a71d29334
Jay Lee <junghwan@princeton.edu>
2016-12-23 12:53:19 -0800

    Added get todos route

--- server/server.js
+++ server/server.js
@@ -19,8 +19,19 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  },
+(error) => {
+    res.status(400).send(error);
+})
 })
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
 });
+
+module.exports = {app};
