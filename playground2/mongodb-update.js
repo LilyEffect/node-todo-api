b8acc4414b99e138d9ef47c9852356e319013145
Jay Lee <junghwan@princeton.edu>
2016-12-22 21:52:55 -0800

    Add POST /todos route and refactor mongoose

--- playground2/mongodb-update.js
+++ playground2/mongodb-update.js
@@ -1,22 +1,24 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+// const {MongoClient, ObjectID} = require("mongodb");
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
-    if (error) {
-      return console.log("Unable to connect to database");
-    }
-    console.log("Connected to MongoDb server.");
+import {MongoClient, ObjectID} from "mongodb";
 
-    db.collection('Users').findOneAndUpdate({
-      _id: new ObjectID("585c4efc1a015c304c12a408")
-    }, {
-      $set: { name: "Jay Lee" },
-      $inc: { age: -1 }
-    },
-      {
-        returnOriginal: false
-      }).then((result) => {console.log(result)});
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (error, db) => {
+	if (error) {
+		return console.log("Unable to connect to database");
+	}
+	console.log("Connected to MongoDb server.");
 
-    db.close();
+	db.collection("Users").findOneAndUpdate({
+		_id: new ObjectID("585c4efc1a015c304c12a408")
+	}, {
+		$set: { name: "Jay Lee" },
+		$inc: { age: -1 }
+	},
+		{
+			returnOriginal: false
+		}).then((result) => {console.log(result);});
+
+	db.close();
 }
 );
