ab22feeddfc07b3aaeb30febf4655f302163110e
Jay Lee <junghwan@princeton.edu>
2016-12-22 20:13:48 -0800

    Added delete and update playground files

--- playground2/mongodb-update.js
+++ playground2/mongodb-update.js
@@ -7,13 +7,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
     }
     console.log("Connected to MongoDb server.");
 
-  //   db.collection('Todos').deleteMany({text: "Practice singing"}).then((result) =>
-  //   {console.log(result);}
-  // )
-
-    db.collection('Users').findOneAndDelete({_id: new ObjectID("585c4d71d129951750637624")}).then((result) =>{
-      console.log(result);
-    })
+    db.collection('Users').findOneAndUpdate({
+      _id: new ObjectID("585c4efc1a015c304c12a408")
+    }, {
+      $set: { name: "Jay Lee" },
+      $inc: { age: -1 }
+    },
+      {
+        returnOriginal: false
+      }).then((result) => {console.log(result)});
 
     db.close();
 }
